695a7e057b4d78433d5060940a5219e2f966e1c4
Zeapherine Islary <zeapherine@gmail.com>
2020-06-16 14:54:41 +0530

    Updates

---
+++ precache-manifest.69654bf0e95d8dd4fa4e13c18b96652a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e765a97ba8959b02c189f3e9e51aefe9",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "dd0e6511cf02b019aaa8",
+    "url": "/monsters-rolodex/static/css/main.32ad53ff.chunk.css"
+  },
+  {
+    "revision": "d0e91b20846f0b628908",
+    "url": "/monsters-rolodex/static/js/2.cccd965e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.cccd965e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd0e6511cf02b019aaa8",
+    "url": "/monsters-rolodex/static/js/main.bdf24ee5.chunk.js"
+  },
+  {
+    "revision": "863086826b6794d683e4",
+    "url": "/monsters-rolodex/static/js/runtime-main.513746bb.js"
+  }
+]);
